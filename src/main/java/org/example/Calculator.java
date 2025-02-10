c31a366ccda91bd028241b74ed7838087fca787a
fatima-amani <1rn21ai048.fatimaamani@gmail.com>
2025-02-10 12:14:58 +0530

    feature: sum and diff

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.ArrayList;
 
 public class Calculator {
-    ArrayList<String> expression;
+    private ArrayList<String> expression;
 
     Calculator(ArrayList<String> expression) {
         this.expression = expression;
@@ -12,24 +12,46 @@ public class Calculator {
     public float calculate() {
 
         divide();
-        multiply();
+        product();
+        sum();
+        difference();
         return Float.parseFloat(expression.getFirst());
     }
 
-    private void divide() {
-        while(expression.size()>1 && expression.contains("/")) {
-            int index = expression.indexOf("/");
-            float curr = Float.parseFloat(expression.get(index-1)) / Float.parseFloat(expression.get((index+1)));
+    private void sum() {
+        while(expression.size()>1 && expression.contains("+")) {
+            int index = expression.indexOf("+");
+            float curr = Float.parseFloat(expression.get(index-1)) + Float.parseFloat(expression.get((index+1)));
             expression.set(index, curr+"");
             expression.remove(index-1);
             expression.remove((index));
         }
     }
 
-    private void multiply() {
+    private void difference() {
+        while(expression.size()>1 && expression.contains("-")) {
+            int index = expression.indexOf("-");
+            float curr = Float.parseFloat(expression.get(index-1)) - Float.parseFloat(expression.get((index+1)));
+            expression.set(index, curr+"");
+            expression.remove(index-1);
+            expression.remove((index));
+        }
+    }
+
+    private void product() {
         while(expression.size()>1 && expression.contains("*")) {
             int index = expression.indexOf("*");
             float curr = Float.parseFloat(expression.get(index-1)) * Float.parseFloat(expression.get((index+1)));
+            expression.set(index, curr+"");
+            expression.remove(index-1);
+            expression.remove((index));
+        }
+    }
+
+    private void divide() {
+        while(expression.size()>1 && expression.contains("/")) {
+            int index = expression.indexOf("/");
+            float curr = Float.parseFloat(expression.get(index-1)) / Float.parseFloat(expression.get((index+1)));
             expression.set(index, curr+"");
             expression.remove(index-1);
             expression.remove((index));
